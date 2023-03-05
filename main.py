69bbccd179689245151c607e624d07a8ce906c94
Yaman Saqqa <abulyomon@gmail.com>
2023-03-05 19:50:50 +0000

    Finished Triangle Law validation

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-def validate_distance_function(f, x, y):
+def validate_distance_function(f, x, y, z):
   if(f(x,y) >= 0 and f(x,x) == 0):
     print("function passes positivity")
     if(f(x,y) == f(y,x)):
       print("function is symmetric")
-      #Triangle law test TBC
-      return True
+      if(f(x,z) <= f(x,y) + f(y,z)):
+        print("function passes Triangle Law")
+        return True
+      else:
+        print("function fails Triangle law")
+        return False
     else:
       print("function is asymmetic")
       return False
@@ -15,10 +19,8 @@ def validate_distance_function(f, x, y):
 normal_distance = lambda x, y: pow(pow((y - x), 2), 0.5)
 control_distance = lambda x, y: x + y
 
-valid = validate_distance_function(normal_distance, 5, 7)
-
+valid = validate_distance_function(normal_distance, 5, 7, 8)
 print ("The function: normal_distance validation as a distance function is:", valid)
 
-valid = validate_distance_function(control_distance, 5, 7)
-
+valid = validate_distance_function(control_distance, 5, 7, 8)
 print ("The function: control_distance validation as a distance function is:", valid)
